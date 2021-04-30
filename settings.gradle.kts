1aa4c28bc5d70fc4093e05806ca7823c6234b7ea
Christian Grach <christian.grach@gmx.at>
2021-04-30 22:03:44 +0200

    remove jcenter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -27,7 +27,6 @@ dependencyResolutionManagement {
         mavenCentral()
         maven { url = uri("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/kotlin-js-wrappers") }
         maven { url = uri("https://maven.pkg.jetbrains.space/public/p/kotlinx-html/maven") }
-        jcenter() // Warning: this repository is going to shut down soon
     }
 }
 
