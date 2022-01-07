0ae029796096873e40a95505787647c7e38b3e8c
Christian Grach <christian.grach@gmx.at>
2022-01-07 11:02:22 +0100

    add 1024 to icns

--- scripts/create_icns.sh
+++ scripts/create_icns.sh
@@ -49,15 +49,16 @@ OUTPUT_ICON_SET_DIR=${CV_TEMP_DIR}CV.iconset
 
 mkdir -p "$OUTPUT_ICON_SET_DIR"
 
-sips -z 16 16    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_16x16.png"
-sips -z 32 32    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_16x16@2x.png"
-sips -z 32 32    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_32x32.png"
-sips -z 64 64    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_32x32@2x.png"
-sips -z 128 128  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_128x128.png"
-sips -z 256 256  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_128x128@2x.png"
-sips -z 256 256  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_256x256.png"
-sips -z 512 512  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_256x256@2x.png"
-sips -z 512 512  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_512x512.png"
+sips -z 16 16      "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_16x16.png"
+sips -z 32 32      "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_16x16@2x.png"
+sips -z 32 32      "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_32x32.png"
+sips -z 64 64      "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_32x32@2x.png"
+sips -z 128 128    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_128x128.png"
+sips -z 256 256    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_128x128@2x.png"
+sips -z 256 256    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_256x256.png"
+sips -z 512 512    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_256x256@2x.png"
+sips -z 512 512    "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_512x512.png"
+sips -z 1024 1024  "$INPUT_FILE_PATH" --out "${OUTPUT_ICON_SET_DIR}/icon_512x512@2x.png"
 
 iconutil -c icns "$OUTPUT_ICON_SET_DIR"
 
