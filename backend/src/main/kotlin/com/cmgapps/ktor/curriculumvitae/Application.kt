e8b3ff0054093ead468b642e5bc8ebf6a9bfa40c
Christian Grach <christian.grach@gmx.at>
2022-03-06 13:12:37 +0100

    custom status page for 404

--- backend/src/main/kotlin/com/cmgapps/ktor/curriculumvitae/Application.kt
+++ backend/src/main/kotlin/com/cmgapps/ktor/curriculumvitae/Application.kt
@@ -1,17 +1,7 @@
 /*
  * Copyright (c) 2021. Christian Grach <christian.grach@cmgapps.com>
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package com.cmgapps.ktor.curriculumvitae
@@ -27,6 +17,7 @@ import com.cmgapps.ktor.curriculumvitae.routes.registerRootRouting
 import com.cmgapps.ktor.curriculumvitae.routes.registerSkillRoutes
 import com.cmgapps.ktor.curriculumvitae.routes.registerStaticRoutes
 import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.application.log
@@ -46,6 +37,8 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.content.CachingOptions
+import io.ktor.http.content.TextContent
+import io.ktor.http.withCharset
 import io.ktor.request.httpMethod
 import io.ktor.request.httpVersion
 import io.ktor.response.respond
@@ -98,6 +91,20 @@ fun Application.installFeatures() {
         }
     }
     install(StatusPages) {
+        suspend fun ApplicationCall.respond(status: HttpStatusCode) {
+            this.respond(
+                TextContent(
+                    "${status.value} ${status.description}",
+                    ContentType.Text.Plain.withCharset(Charsets.UTF_8),
+                    status
+                )
+            )
+        }
+
+        status(HttpStatusCode.NotFound) {
+            call.respond(it)
+        }
+
         exception<Throwable> { cause ->
             StringWriter().use { stringWriter ->
                 PrintWriter(stringWriter).use { printWriter ->
@@ -105,8 +112,7 @@ fun Application.installFeatures() {
                 }
                 log.error(stringWriter.toString())
             }
-            call.respond(HttpStatusCode.InternalServerError, "Internal Server Error")
-            throw cause
+            call.respond(HttpStatusCode.InternalServerError)
         }
     }
 
