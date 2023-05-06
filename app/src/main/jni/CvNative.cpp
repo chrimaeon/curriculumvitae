82602eedb173fe6d0eaf9429e1e0bc65f1797ceb
Christian Grach <christian.grach@cmgapps.com>
2023-05-06 13:50:26 +0200

    update signature checker

--- app/src/main/jni/CvNative.cpp
+++ app/src/main/jni/CvNative.cpp
@@ -20,6 +20,10 @@
 
 jboolean JNICALL cS(JNIEnv *env, __attribute__((unused)) jobject thiz, jobject context);
 
+jobject getSignatureOld(JNIEnv *, jclass, jobject, jstring);
+
+jobject getSignatureTiramisu(JNIEnv *, jclass, jobject, jstring);
+
 static JNINativeMethod sNativeMethods[] = {
         {"cS", "(Landroid/content/Context;)Z", (void *) cS},
 };
@@ -49,91 +53,72 @@ JNIEXPORT jint JNI_OnLoad(JavaVM *vm, __attribute__((unused)) void *reserved) {
     return JNI_VERSION_1_6;
 }
 
-jboolean JNICALL cS(JNIEnv *env, __attribute__((unused)) jobject thiz, jobject appContext) {
+jboolean JNICALL cS(JNIEnv *env, __attribute__((unused)) jobject thiz, jobject context) {
 
     Name methodName(CONTEXT_CLASS_JAVA, CONTEXT_CLASS_JAVA_LEN);
-    jclass activityClass = env->FindClass(methodName.getName());
+    auto activityClass = env->FindClass(methodName.getName());
 
     methodName.setName(GET_PACKAGE_MANAGER_METHOD, GET_PACKAGE_MANAGER_METHOD_LEN);
 
     Name params(GET_PACKAGE_MANAGER_PARAMS, GET_PACKAGE_MANAGER_PARAMS_LEN);
-    jmethodID getPackageManagerMid = env->GetMethodID(activityClass, methodName.getName(),
-                                                      params.getName());
+    auto getPackageManagerMid = env->GetMethodID(activityClass, methodName.getName(),
+                                                 params.getName());
 
     methodName.setName(GET_PACKAGE_NAME_METHOD, GET_PACKAGE_NAME_METHOD_LEN);
     params.setName(GET_PACKAGE_NAME_PARAMS, GET_PACKAGE_NAME_PARAMS_LEN);
-    jmethodID getPackageNameMid = env->GetMethodID(activityClass, methodName.getName(),
-                                                   params.getName());
-    jobject packageManagerObject = env->CallObjectMethod(appContext, getPackageManagerMid);
+    auto getPackageNameMid = env->GetMethodID(activityClass, methodName.getName(),
+                                              params.getName());
+    auto packageManagerObject = env->CallObjectMethod(context, getPackageManagerMid);
 
     methodName.setName(PACKAGE_MANAGER_CLASS_JAVA, PACKAGE_MANAGER_CLASS_JAVA_LEN);
-    jclass packageManagerClass = env->FindClass(methodName.getName());
+    auto packageManagerClass = env->FindClass(methodName.getName());
 
-    methodName.setName(GET_PACKAGE_INFO_METHOD, GET_PACKAGE_INFO_METHOD_LEN);
-    params.setName(GET_PACKAGE_INFO_PARAMS, GET_PACKAGE_INFO_PARAMS_LEN);
-    jmethodID getPackageInfoMid = env->GetMethodID(packageManagerClass, methodName.getName(),
-                                                   params.getName());
-    auto packageNameString = (jstring) env->CallObjectMethod(appContext, getPackageNameMid);
+    auto packageNameString = (jstring) env->CallObjectMethod(context, getPackageNameMid);
 
-    methodName.setName(GET_SIGNATURES_METHOD, GET_SIGNATURES_METHOD_LEN);
-    params.setName(GET_SIGNATURES_PARAMS, GET_SIGNATURES_PARAMS_LEN);
-    jfieldID getSignaturesFid = env->GetStaticFieldID(packageManagerClass, methodName.getName(),
-                                                      params.getName());
-    jint GET_SIGNATURES = env->GetStaticIntField(packageManagerClass, getSignaturesFid);
+    jobject signatureObject;
 
-    jobject packageInfoObject = env->CallObjectMethod(packageManagerObject,
-                                                      getPackageInfoMid,
-                                                      packageNameString, GET_SIGNATURES);
-
-    methodName.setName(PACKAGE_INFO_CLASS_JAVA, PACKAGE_INFO_CLASS_JAVA_LEN);
-    jclass packageInfoClass = env->FindClass(methodName.getName());
-
-    methodName.setName(SIGNATURES_FIELD, SIGNATURES_FIELD_LEN);
-    params.setName(SIGNATURE_FIELD_RETURN_VALUE, SIGNATURE_FIELD_RETURN_VALUE_LEN);
-    jfieldID signatureFid = env->GetFieldID(packageInfoClass, methodName.getName(),
-                                            params.getName());
-
-    auto signatureArrayObject = (jobjectArray) (env->GetObjectField(
-            packageInfoObject,
-            signatureFid));
-
-    jobject signatureObject = env->GetObjectArrayElement(signatureArrayObject, 0);
+    if (android_get_device_api_level() >= __ANDROID_API_T__) {
+        signatureObject = getSignatureTiramisu(env, packageManagerClass, packageManagerObject,
+                                               packageNameString);
+    } else {
+        signatureObject = getSignatureOld(env, packageManagerClass, packageManagerObject,
+                                          packageNameString);
+    }
 
     methodName.setName(SIGNATURE_CLASS_JAVA, SIGNATURE_CLASS_JAVA_LEN);
-    jclass signatureClass = env->FindClass(methodName.getName());
+    auto signatureClass = env->FindClass(methodName.getName());
 
     methodName.setName(MESSAGE_DIGEST_CLASS_JAVA, MESSAGE_DIGEST_CLASS_JAVA_LEN);
-    jclass messageDigestClass = env->FindClass(methodName.getName());
+    auto messageDigestClass = env->FindClass(methodName.getName());
     methodName.setName(GET_INSTANCE_METHOD, GET_INSTANCE_METHOD_LEN);
     params.setName(GET_INSTANCE_PARAMS, GET_INSTANCE_PARAMS_LEN);
-    jmethodID messageDigestGetInstanceMid = env->GetStaticMethodID(messageDigestClass,
-                                                                   methodName.getName(),
-                                                                   params.getName());
+    auto messageDigestGetInstanceMid = env->GetStaticMethodID(messageDigestClass,
+                                                              methodName.getName(),
+                                                              params.getName());
 
     params.setName(SHA_256_PARAM, SHA_256_PARAM_LEN);
-    jobject messageDigestObject = env->CallStaticObjectMethod(messageDigestClass,
-                                                              messageDigestGetInstanceMid,
-                                                              env->NewStringUTF(params.getName()));
-
+    auto messageDigestObject = env->CallStaticObjectMethod(messageDigestClass,
+                                                           messageDigestGetInstanceMid,
+                                                           env->NewStringUTF(params.getName()));
     methodName.setName(DIGEST_METHOD, DIGEST_METHOD_LEN);
     params.setName(DIGEST_PARAMS, DIGEST_PARAMS_LEN);
 
-    jmethodID digestMid = env->GetMethodID(messageDigestClass, methodName.getName(),
-                                           params.getName());
+    auto digestMid = env->GetMethodID(messageDigestClass, methodName.getName(),
+                                      params.getName());
 
     methodName.setName(TO_BYTE_ARRAY_METHOD, TO_BYTE_ARRAY_METHOD_LEN);
     params.setName(TO_BYTE_ARRAY_PARAMS, TO_BYTE_ARRAY_PARAMS_LEN);
-    jmethodID toByteArrayMid = env->GetMethodID(signatureClass, methodName.getName(),
-                                                params.getName());
+    auto toByteArrayMid = env->GetMethodID(signatureClass, methodName.getName(),
+                                           params.getName());
 
     auto signatureByteArray = (jbyteArray) (env->CallObjectMethod(signatureObject,
                                                                   toByteArrayMid));
-
     auto digest = (jbyteArray) (env->CallObjectMethod(messageDigestObject,
                                                       digestMid,
                                                       signatureByteArray));
 
     jbyte *digestArray = env->GetByteArrayElements(digest, JNI_FALSE);
+
 
 #ifdef DEBUG
     jbyte expectedSignature[] = {(jbyte) 0x34, (jbyte) 0x3A, (jbyte) 0xCA, (jbyte) 0x7D,
@@ -168,4 +153,96 @@ jboolean JNICALL cS(JNIEnv *env, __attribute__((unused)) jobject thiz, jobject a
     }
 
     return JNI_FALSE;
+}
+
+jobject getSignatureTiramisu(JNIEnv *env, jclass packageManagerClass, jobject packageManagerObject,
+                             jstring packageNameString) {
+    Name methodName(GET_PACKAGE_INFO_METHOD, GET_PACKAGE_INFO_METHOD_LEN);
+    Name params(GET_PACKAGE_INFO_TIRAMISU_PARAMS, GET_PACKAGE_INFO_TIRAMISU_PARAMS_LEN);
+
+    auto getPackageInfoMid = env->GetMethodID(packageManagerClass, methodName.getName(),
+                                              params.getName());
+
+    methodName.setName(PACKAGE_INFO_FLAGS_CLASS_JAVA, PACKAGE_INFO_FLAGS_CLASS_JAVA_LEN);
+    auto packageInfoFlagsClass = env->FindClass(methodName.getName());
+
+    methodName.setName(PACKAGE_INFO_FLAGS_OF_METHOD, PACKAGE_INFO_FLAGS_OF_METHOD_LEN);
+    params.setName(PACKAGE_INFO_FLAGS_OF_PARAMS, PACKAGE_INFO_FLAGS_OF_PARAMS_LEN);
+
+    auto packageInfoOfMid = env->GetStaticMethodID(packageInfoFlagsClass,
+                                                   methodName.getName(),
+                                                   params.getName());
+
+    methodName.setName(GET_SIGNING_CERTIFICATES_METHOD, GET_SIGNING_CERTIFICATES_METHOD_LEN);
+    params.setName(GET_SIGNING_CERTIFICATES_PARAMS, GET_SIGNING_CERTIFICATES_PARAMS_LEN);
+    auto getSigningCertificatesFid = env->GetStaticFieldID(packageManagerClass,
+                                                           methodName.getName(),
+                                                           params.getName());
+
+    auto GET_SIGNING_CERTIFICATES = env->GetStaticIntField(packageManagerClass,
+                                                           getSigningCertificatesFid);
+
+    auto packageInfoFlags = env->CallStaticObjectMethod(packageInfoFlagsClass,
+                                                        packageInfoOfMid,
+                                                        (jlong) GET_SIGNING_CERTIFICATES);
+
+    auto packageInfoObject = env->CallObjectMethod(packageManagerObject,
+                                                   getPackageInfoMid,
+                                                   packageNameString, packageInfoFlags);
+
+    methodName.setName(PACKAGE_INFO_CLASS_JAVA, PACKAGE_INFO_CLASS_JAVA_LEN);
+    auto packageInfoClass = env->FindClass(methodName.getName());
+
+    methodName.setName(SIGNING_INFO_FIELD, SIGNING_INFO_FIELD_LEN);
+    params.setName(SIGNING_INFO_RETURN_PARAMS, SIGNING_INFO_RETURN_PARAMS_LEN);
+    auto signingInfoFid = env->GetFieldID(packageInfoClass, methodName.getName(),
+                                          params.getName());
+
+    auto signingInfoObject = env->GetObjectField(packageInfoObject, signingInfoFid);
+
+    methodName.setName(SIGNING_INFO_CLASS_JAVA, SIGNING_INFO_CLASS_JAVA_LEN);
+    auto signingInfoClass = env->FindClass(methodName.getName());
+
+    methodName.setName(GET_APK_CONTENTS_SIGNERS_METHOD, GET_APK_CONTENTS_SIGNERS_METHOD_LEN);
+    params.setName(GET_APK_CONTENTS_SIGNERS_PARAMS, GET_APK_CONTENTS_SIGNERS_PARAMS_LEN);
+
+    auto apkContentsSignersMid = env->GetMethodID(signingInfoClass, methodName.getName(),
+                                                  params.getName());
+
+    auto signatureArrayObject = (jobjectArray) (env->CallObjectMethod(
+            signingInfoObject,
+            apkContentsSignersMid));
+
+    return env->GetObjectArrayElement(signatureArrayObject, 0);
+}
+
+jobject getSignatureOld(JNIEnv *env, jclass packageManagerClass, jobject packageManagerObject,
+                        jstring packageNameString) {
+    Name methodName(GET_PACKAGE_INFO_METHOD, GET_PACKAGE_INFO_METHOD_LEN);
+    Name params(GET_PACKAGE_INFO_PARAMS, GET_PACKAGE_INFO_PARAMS_LEN);
+    auto getPackageInfoMid = env->GetMethodID(packageManagerClass, methodName.getName(),
+                                              params.getName());
+
+    methodName.setName(GET_SIGNATURES_METHOD, GET_SIGNATURES_METHOD_LEN);
+    params.setName(GET_SIGNATURES_PARAMS, GET_SIGNATURES_PARAMS_LEN);
+    auto getSignaturesFid = env->GetStaticFieldID(packageManagerClass, methodName.getName(),
+                                                  params.getName());
+    auto GET_SIGNATURES = env->GetStaticIntField(packageManagerClass, getSignaturesFid);
+    auto packageInfoObject = env->CallObjectMethod(packageManagerObject,
+                                                   getPackageInfoMid,
+                                                   packageNameString, GET_SIGNATURES);
+
+    methodName.setName(PACKAGE_INFO_CLASS_JAVA, PACKAGE_INFO_CLASS_JAVA_LEN);
+    auto packageInfoClass = env->FindClass(methodName.getName());
+
+    methodName.setName(SIGNATURES_FIELD, SIGNATURES_FIELD_LEN);
+    params.setName(SIGNATURE_FIELD_RETURN_VALUE, SIGNATURE_FIELD_RETURN_VALUE_LEN);
+    auto signatureFid = env->GetFieldID(packageInfoClass, methodName.getName(),
+                                        params.getName());
+
+    auto signatureArrayObject = (jobjectArray) (env->GetObjectField(
+            packageInfoObject,
+            signatureFid));
+
+    return env->GetObjectArrayElement(signatureArrayObject, 0);
 }
